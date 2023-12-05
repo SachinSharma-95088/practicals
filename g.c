88c817a43a8c358df164930f4e09af0a090f7dcf
SachinSharma-95088 <149468744+SachinSharma-95088@users.noreply.github.com>
2023-12-05 22:40:20 +0530

    Add files via upload

---
+++ g.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char a[10]= "Hello";
+    printf("%d",strlen(a));
+    char b[50]="Hello";
+    
+  
+
+    printf("%s",strcat(b,a));
+
+}
--- l.c
+++ l.c
@@ -1,43 +1,15 @@
-#include <stdio.h>
-int main (){
-    int a[3][3] ,b[3][3] ,c[3][3];
+#include<stdio.h>
+int fn(int n){
+    if (n>1);
+    return n*fn(n-1);
 
-    int m,n,p,q,i,j,k;
-     
-    for(i=0;i<3;i++){
-    for(j=0;j<3;j++){
-        a[i][j]= (i+1)*3+9*j*((i+1)*3) ;
-        b[i][j]= 3 *(i+1)*6+(i+j+1)*2*(j+1);
-        printf("%d ",a[i][j]);
-    } 
-    printf(" \n");
-    }
+}
 
+int main(){
+    int b;
+    b=10;
+    printf("%d",fn(b));
 
-    for(i=0;i<3;i++){
-    for(j=0;j<3;j++){
-        a[i][j]= (i+1)*3+9*j*((i+1)*3) ;
-        b[i][j]= 3 *(i+1)*6+(i+j+1)*2*(j+1);
-        printf("%d ",b[i][j]);
-    } 
-    printf(" \n");
-    }
-    if (n==p){
-        for (i=0;i<m;i++){
-            for (j=0;j<q;j++){
-                c[i][j]=0;
-                {
-                    for(k=0;k<p;k++){
-                        c [i][j] += a[i][k]*b[k][j];
-                        
-                    }
-                    printf ("%d",c[i][j]);
-                }
-                printf("\n");
-            }
-        }
-
-    }
-    return 0;
-
+    
+    
 }
